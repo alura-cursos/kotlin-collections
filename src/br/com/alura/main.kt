e22e621a721d1fc1b6a0d07c0f4931222244839f
Alex Felipe <alexfelipevieira@gmail.com>
2020-05-29 10:53:11 -0300

    1.3

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,14 +1,35 @@
 package br.com.alura
 
 fun main() {
-    val nomes: List<String> = listOf(
+    val banco = BancoDeNomes()
+//    banco.nomes.add("Alex")
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String){
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
             "Alex",
             "Fran",
             "Gui",
             "Maria",
             "Ana"
     )
-    for (nome in nomes){
+    for (nome in nomes) {
         println(nome)
     }
     println(nomes)
