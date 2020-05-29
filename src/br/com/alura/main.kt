f915fa3bc11eb852886a675191b26f8fc4503b96
Alex Felipe <alexfelipevieira@gmail.com>
2020-05-29 09:23:54 -0300

    1.2

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,17 @@
 package br.com.alura
 
 fun main() {
-
+    val nomes: List<String> = listOf(
+            "Alex",
+            "Fran",
+            "Gui",
+            "Maria",
+            "Ana"
+    )
+    for (nome in nomes){
+        println(nome)
+    }
+    println(nomes)
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção ${nomes.size}")
 }
