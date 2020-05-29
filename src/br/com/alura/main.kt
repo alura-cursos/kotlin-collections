69bf28fc6b8fe158c6ff6c47e2746b55a67e9cf8
Alex Felipe <alexfelipevieira@gmail.com>
2020-05-29 12:19:01 -0300

    1.4

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,15 +2,16 @@ package br.com.alura
 
 fun main() {
     val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
 //    banco.nomes.add("Alex")
     banco.salva("Alex")
-    println(banco.nomes)
+    println(nomesSalvos)
     println(BancoDeNomes().nomes)
 }
 
 class BancoDeNomes {
 
-    val nomes: Collection<String> get() = dados
+    val nomes: Collection<String> get() = dados.toList()
 
     fun salva(nome: String){
         dados.add(nome)
