7898bb6c39e62d580cd3f04ad5f4a8360c73f0ae
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-01 09:28:04 -0300

    2.2

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,8 +1,13 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println(assistiramAmbos.distinct())
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
 }
