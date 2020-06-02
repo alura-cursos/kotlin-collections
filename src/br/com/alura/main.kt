a457c75ff9fbbb4d84b80cf7778c654c3993f8af
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-02 13:24:30 -0300

    4.2

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -6,7 +6,7 @@ fun main() {
             Pair(2, 64.0),
             3 to 50.0,
             4 to 100.0,
-            5 to 150.0,
+            5 to 100.0,
             6 to 80.0
     )
 //    val valorPedido = pedidos.getValue(5)
@@ -45,6 +45,28 @@ fun main() {
         numero % 2 == 0
     }
     println(pedidosPares)
+
+    println(pedidos + mapOf(7 to 90.0, 8 to 20.0))
+    println(pedidos)
+
+    println(pedidos - listOf(6, 5))
+    println(pedidos)
+
+//    pedidos.putAll(listOf<Pair<Int,Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0))
+    pedidos += listOf<Pair<Int,Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0)
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(50.0)
+    println(pedidos)
+
+    pedidos.values.remove(100.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
 }
 
 fun testaMap(pedidos: MutableMap<Int, Double>) {
