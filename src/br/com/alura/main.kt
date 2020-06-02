0241f52309b9aaf1d22b85570fcbcc6ecf066a4d
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-02 10:21:06 -0300

    4.1

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -3,15 +3,57 @@ package br.com.alura
 fun main() {
     val pedidos: MutableMap<Int, Double> = mutableMapOf(
             Pair(1, 20.0),
-            Pair(2, 34.0),
-            3 to 50.0
+            Pair(2, 64.0),
+            3 to 50.0,
+            4 to 100.0,
+            5 to 150.0,
+            6 to 80.0
     )
+//    val valorPedido = pedidos.getValue(5)
+//    println(valorPedido)
+    val mensagem: Double? = pedidos.getOrElse(0, {
+        0.0
+    })
+    println(mensagem)
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+
+    for (numero in pedidos.keys){
+        println("Pedido $numero")
+    }
+
+    println(pedidos.values)
+
+    for (valor in pedidos.values){
+        println("Valor do pedido: $valor")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 70.0
+    }
+
+    println(pedidosFiltrados)
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+    println(pedidosAcima)
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+    println(pedidosPares)
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     println(pedidos)
     val pedido = pedidos[3]
     pedido?.let {
         println("pedido $it")
     }
-    for (p: Map.Entry<Int, Double> in pedidos){
+    for (p: Map.Entry<Int, Double> in pedidos) {
         println("número do pedido: ${p.key}")
         println("valor do pedido: ${p.value}")
     }
