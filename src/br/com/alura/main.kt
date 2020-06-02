33cc71685be8b96787452bc7d9cf52cc0c670409
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-02 09:30:08 -0300

    3.2

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,14 +1,33 @@
 package br.com.alura
 
 fun main() {
-    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+            Pair(1, 20.0),
+            Pair(2, 34.0),
+            3 to 50.0
+    )
     println(pedidos)
     val pedido = pedidos[3]
     pedido?.let {
         println("pedido $it")
     }
-    for (pedido: Map.Entry<Int, Double> in pedidos){
-        println("número do pedido: ${pedido.key}")
-        println("valor do pedido: ${pedido.value}")
+    for (p: Map.Entry<Int, Double> in pedidos){
+        println("número do pedido: ${p.key}")
+        println("valor do pedido: ${p.value}")
     }
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 50.0)
+    println(pedidos)
 }
