8ab319c3757e8e2b5e7d6ba05ec2692e19e0e4a6
Alex Felipe <alexfelipevieira@gmail.com>
2020-06-03 10:31:44 -0300

    5.2

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -5,7 +5,8 @@ fun main() {
             Pedido(1, 20.0),
             Pedido(2, 60.0),
             Pedido(3, 30.0),
-            Pedido(4, 70.0)
+            Pedido(4, 70.0),
+            Pedido(5, 40.0)
     )
     println(pedidos)
     val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido ->
@@ -18,8 +19,37 @@ fun main() {
         pedido.valor > 50.0
     }
     println(pedidosFreteGratis)
-    println(pedidosFreteGratis[Pedido(numero=2, valor=60.0)])
+    println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)])
 
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
+
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> =
+            pedidos.groupBy { pedido: Pedido ->
+                pedido.valor > 50.0
+            }
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[false])
+
+    val nomes = listOf(
+            "Alex",
+            "Fran",
+            "Gui",
+            "Ana",
+            "Paulo",
+            "Maria",
+            "Mario",
+            "Gisele"
+    )
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+
+    println(agenda)
+    println(agenda['A'])
 }
 
 data class Pedido(val numero: Int, val valor: Double)
